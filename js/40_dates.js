861fb4b6a0f670a760bcc0ea75d912d6ba560e41
Douglas <drab.yaroslaw@gmail.com>
2021-02-10 13:54:31 +0200

    dates.js added

---
+++ js/40_dates.js
@@ -0,0 +1,28 @@
+const now = new Date();
+console.log(now);
+
+// METHODS
+// Getting the date's components
+console.log(now.getFullYear());
+console.log(now.getMonth() + 1);
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getHours());
+console.log(now.getUTCHours());
+console.log(now.getTimezoneOffset());   // in minutes
+console.log(now.getTime());     // milliseconds since 1970
+
+// set methods
+console.log(now.setHours(18));
+console.log(now);
+
+
+let start = new Date();
+
+for (let i = 0; i < 100000000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Loop competed for ${end - start} ms.`);
